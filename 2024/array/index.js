9a2263360030707abd39ed3fa71527ae70f701be
Naveen <naveen.ndr97@gmail.com>
2024-06-03 11:20:56 +0530

    observerbility

---
+++ 2024/array/index.js
@@ -0,0 +1,15 @@
+const months = ["Jan", "March", "April", "May"];
+// months.splice(1, 0, "Feb");
+// months.splice(2, 1, "Sep");
+// months.splice(2, 2, "Sep");
+months.splice(2, 2, "Sep");
+// console.log(months);
+
+const str = "naveen chadnar";
+let modifieldStr = "";
+
+for (let i = 0; i < str.length; i++) {
+    if (i % 2 !== 0) modifieldStr += str[i].toUpperCase();
+    else modifieldStr += str[i];
+}
+console.log(modifieldStr);
