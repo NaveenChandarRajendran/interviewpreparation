b118cc5398ccaa1a0b2129ee02bc607e5a49cff2
Naveen <naveen.ndr97@gmail.com>
2024-06-04 14:05:22 +0530

    prototype

---
+++ 2024/prototype/mapFIlterReduce.js
@@ -0,0 +1,34 @@
+//Map 
+let arr = [1, 2, 3];
+arr.map((each, index, arr) => console.log(each, index, arr));
+Array.prototype.naveenMap = function (cb) {
+    let res = []; for (let i = 0; i < this.length; i++) {
+        res.push(cb(this[i], i, this))
+    } return res;
+}
+arr.naveenMap((each) => console.log("nav", each));
+
+//Filter 
+Array.prototype.naveenFilter = function (cb) {
+    let res = [];
+    for (let i = 0; i < this.length; i++) {
+        if (cb(this[i], i)) {
+            res.push(this[i]);
+        }
+    } return res;
+}
+const customFilterValue = name.naveenFilter((each) => each !== 'two');
+console.log("filter", customFilterValue); const addNo = [1, 2, 3, 4];
+
+
+//Reduce
+const add = addNo.reduce((acc, curr) => {
+    return acc + curr;
+}, 0)
+Array.prototype.naveenReduce = function (cb, initialValue) {
+    for (let i = 0; i < this.length; i++) {
+        initialValue = cb(initialValue, this[i])
+    } return initialValue;
+}
+const customReduce = addNo.naveenReduce((acc, curr) => { return acc + curr; }, 0)
+console.log("customReduce", customReduce);
